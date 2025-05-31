917aa42aa83553caeedfb4c6c197612d735865e0
arcardio <kingsbee@icloud.com>
2025-05-30 22:12:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-
 function enterSite() {
-  document.querySelector('.entrance').style.display = 'none';
-  document.querySelector('.main-content').classList.remove('hidden');
-}
-
-function showSection(sectionId) {
-  document.querySelectorAll('.info-section').forEach(el => el.classList.add('hidden'));
-  document.getElementById(sectionId).classList.remove('hidden');
+  document.getElementById('entrance').style.display = 'none';
+  document.getElementById('mainContent').classList.remove('hidden');
 }
